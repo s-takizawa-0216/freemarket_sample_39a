c916f4191c41e9f2146322e2dc4b2c6e8aeb7fb9
yuuki <devel.yuuki@gmail.com>
2018-12-01 14:47:43 +0900

    Fivecommit

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "freemarket_sample_39a"
-set :repo_url, "git@example.com:yuuki999/freemarket_sample_39a.git"
+set :repo_url, "git@github.com:yuuki999/freemarket_sample_39a.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
