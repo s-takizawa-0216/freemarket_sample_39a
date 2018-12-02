a492b3ed6907aa3e349eef2272c7fc37fabda664
yuuki <devel.yuuki@gmail.com>
2018-12-02 21:17:48 +0900

    A

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -59,3 +59,7 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+
+before_exec do |server|
+  ENV["BUNDLE_GEMFILE"] = File.join(File.expand_path("../../../../", __FILE__), "current", "Gemfile")
+end
