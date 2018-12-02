4d95bee35f2061aa558e242e815a6af52ab9b028
yuuki <devel.yuuki@gmail.com>
2018-12-02 21:23:18 +0900

    Aaa

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -59,7 +59,8 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+app_path = File.expand_path('../../../', __FILE__)
 
 before_exec do |server|
-  ENV["BUNDLE_GEMFILE"] = File.join(File.expand_path("../../../../", __FILE__), "current", "Gemfile")
+  ENV["BUNDLE_GEMFILE"] = "#{app_path}/current/Gemfile"
 end
