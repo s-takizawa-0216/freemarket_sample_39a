13bcbacd6e0bfc5b1b2c4f0045ed1a911aaf59da
yuuki <devel.yuuki@gmail.com>
2018-12-02 21:07:16 +0900

    E

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -63,6 +63,3 @@
 
 
 server '52.193.94.241', user: 'ec2-user', roles: %w{app db web}
-before_exec do |server|
-  ENV["BUNDLE_GEMFILE"] = File.join(File.expand_path("../../../../", __FILE__), "current", "Gemfile")
-end
