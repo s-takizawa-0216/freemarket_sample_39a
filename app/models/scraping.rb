d77c62edc1ef404fcc47f2129dcff657b289e6da
yuuki <devel.yuuki@gmail.com>
2018-12-03 17:38:10 +0900

    Yamanosann

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -18,4 +18,25 @@ class Scraping
      brand.save
 #    puts name
   end
+
+  def self.large_category  # 大カテゴリーのテキストを取得、テーブルに保存
+   # require ‘Mechanize’
+
+   agent = Mechanize.new
+   names = []
+   page = agent.get("https://www.mercari.com/jp/category/")
+   elements = page.search(" .category-list-individual-box-sub-sub-category-name a")
+   elements.each do |ele|
+     names << ele.inner_text
+   end
+   names.each do |name|
+     save_large_category(name)
+   end
+ end
+
+ def self.save_large_category(str)
+   name = str.gsub(" ", "").gsub("すべて", "")
+   lcategory = Lcategory.new(name: name)#.first_or_initialize
+   lcategory.save
+ end
 end
