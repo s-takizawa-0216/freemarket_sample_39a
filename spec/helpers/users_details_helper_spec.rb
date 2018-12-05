39ff5682bf3c713d95f2f67eea4b02fd927821d0
yuuki <devel.yuuki@gmail.com>
2018-12-05 19:41:39 +0900

    Create spec.rb
    コントローラを作った時にできるファイル

--- spec/helpers/users_details_helper_spec.rb
+++ spec/helpers/users_details_helper_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 # Specs in this file have access to a helper object that includes
-# the UsersDetailHelper. For example:
+# the UsersDetailsHelper. For example:
 #
-# describe UsersDetailHelper do
+# describe UsersDetailsHelper do
 #   describe "string concat" do
 #     it "concats two strings with spaces" do
 #       expect(helper.concat_strings("this","that")).to eq("this that")
 #     end
 #   end
 # end
-RSpec.describe UsersDetailHelper, type: :helper do
+RSpec.describe UsersDetailsHelper, type: :helper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
