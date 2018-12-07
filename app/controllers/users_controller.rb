798d31bfda8ab94eaf9d95647cf691e77a7caea3
yuuki <devel.yuuki@gmail.com>
2018-12-07 14:00:18 +0900

    Edit users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+  def create
+  end
+  def new2
+    @user = User.new(user_params)
+    render :new if @user.invalid?
+  end
+  def new1
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name,:email)
+  end
 end
