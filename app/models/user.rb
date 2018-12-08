e1f15d1010d5ed13f27f5b2e39c19d0478ee3336
shun <s.takizawa.0216@gmail.com>
2018-12-08 15:30:46 +0900

    edit user.rb (model)
    snsログイン用にuserモデルを編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,35 @@ class User < ApplicationRecord
    # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
+
+# グーグルログインの実装部分
+   def self.find_for_google_oauth2(auth)
+    user = User.where(email: auth.info.email).first
+
+    unless user
+      user = User.create(name:     auth.info.name,
+                         provider: auth.provider,
+                         uid:      auth.uid,
+                         email:    auth.info.email,
+                         token:    auth.credentials.token,
+                         password: Devise.friendly_token[0, 20])
+    end
+    user
+  end
+
+# facebookログインの実装部分
+ def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      user = User.create(name:auth.extra.raw_info.name,
+                          provider:auth.provider,
+                          uid:auth.uid,
+                          email:auth.info.email,
+                          password:Devise.friendly_token[0,20])
+    end
+    user
+  end
 
 # アソシエーションの定義
   has_one :users_details, dependent: :destroy
@@ -18,3 +46,4 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 end
+
