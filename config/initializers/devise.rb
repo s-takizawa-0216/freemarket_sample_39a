a35e06faabf65b8061d97df35d24f3a72eddd5ea
shun <s.takizawa.0216@gmail.com>
2018-12-08 17:41:37 +0900

    correction devise.rb
    軽微な修正

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -258,14 +258,14 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
-    config.omniauth :google_oauth2,
-      Rails.application.secrets.google_client_id,
-      Rails.application.secrets.google_client_secret
+  config.omniauth :google_oauth2,
+    Rails.application.secrets.google_client_id,
+    Rails.application.secrets.google_client_secret
 
 
   config.omniauth :facebook,
-                  Rails.application.secrets.facebook_app_id,
-                  Rails.application.secrets.facebook_api_secret
+    Rails.application.secrets.facebook_app_id,
+    Rails.application.secrets.facebook_api_secret
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
