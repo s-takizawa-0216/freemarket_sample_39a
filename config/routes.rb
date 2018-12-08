2aae49aed24e98cfb560555582aa495c704b9fe4
shun <s.takizawa.0216@gmail.com>
2018-12-08 15:38:40 +0900

    edit routes.rb
    snsログイン機能の実装の為、deviseのルーティングに
    omniauthのcallbackを読み込む用に追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
+
   root 'mercaries#index'
   resources :mercaries
   resources :items
