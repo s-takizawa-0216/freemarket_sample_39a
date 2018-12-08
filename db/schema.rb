08c98dee308a205e54f120437e87516df37c8070
shun <s.takizawa.0216@gmail.com>
2018-12-08 15:42:02 +0900

    edit  schema.rb
    snsログイン用にマイグレーションファイル を設定した為。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_06_123535) do
+ActiveRecord::Schema.define(version: 2018_12_08_041211) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -129,12 +129,15 @@ ActiveRecord::Schema.define(version: 2018_12_06_123535) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "name", null: false
-    t.string "image"
+    t.string "image", default: "0"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
