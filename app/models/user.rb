368df21eb9203e7d3a5cab496dd64d80d83f9fbd
Ko Watanabe <lsbean82@gmail.com>
2018-12-10 20:51:24 +0900

    Edit Association for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,11 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_one :credit_card, dependent: :destroy
   has_many :evaluations, dependent: :destroy
+  #出品機能に応じて、取引中、売却済みの定義を付け加えました。
+  has_many :buyed_items, foreign_key: "buyer_id", class_name: "Item"
+  has_many :saling_items, -> { where("buyer_id is NULL") }, foreign_key: "saler_id", class_name: "Item"
+  has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "saler_id", class_name: "Item"
+
 
 # バリデーションの定義
   validates :name, presence: true , uniqueness: true   #空でない事、nameが重複しない事
