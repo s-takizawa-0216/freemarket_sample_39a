13adb1c9ad1285801e64977aab5920ae887bd2d7
Ko Watanabe <lsbean82@gmail.com>
2018-12-11 14:03:46 +0900

    Edit association for item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,13 @@
 class Item < ApplicationRecord
   # アソシエーションの定義
-  belongs_to :user
+  # belongs_to :user
   belongs_to :lcategory
   belongs_to :mcategory
   belongs_to :scategory
   belongs_to :saler, class_name: "User"
-  belongs_to :buyer, class_name: "User"
-  has_many :brands
-  has_one :shipping_method
-  has_many :likes
-  has_many :images
+  # belongs_to :buyer, class_name: "User"
+  # has_many :brands
+  # has_one :shipping_method
+  # has_many :likes
+  # has_many :images
 end
