648a0e2072c758d255a81cc5527631d15665a07f
Ko Watanabe <lsbean82@gmail.com>
2018-12-11 21:26:05 +0900

    Edit association

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,10 +4,11 @@ class Item < ApplicationRecord
   belongs_to :lcategory
   belongs_to :mcategory
   belongs_to :scategory
-  belongs_to :saler, class_name: "User"
+  # belongs_to :saler, class_name: "User"
   # belongs_to :buyer, class_name: "User"
   # has_many :brands
-  # has_one :shipping_method
+  has_one :shipping_method
+  accepts_nested_attributes_for :shipping_method
   # has_many :likes
   # has_many :images
 end
