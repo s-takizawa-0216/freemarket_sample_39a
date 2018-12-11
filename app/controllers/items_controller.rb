f58ef04f74db622c1294a2e228e2e6a064783354
Ko Watanabe <lsbean82@gmail.com>
2018-12-11 14:03:26 +0900

    Edit item controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,23 +1,20 @@
 class ItemsController < ApplicationController
 
   def index
-<<<<<<< HEAD
     @items = Item.all
-=======
-    @items = Item.order("created_at DESC")
->>>>>>> yuuki999/master
   end
 
   def show
   end
 
   def create
-    @item = Item.new(name: item_params[:name],price: item_params[:price], detail: item_params[:detail],condition: item_params[:condition])
+    @item = Item.new(name: item_params[:name], price: item_params[:price], detail: item_params[:detail], condition: item_params[:condition])
     @item.save
   end
 
   def new
-    @item = Item.new
+    @item = Item.new(name: item_params[:name], price: item_params[:price], detail: item_params[:detail], condition: item_params[:condition],size: item_params[:size])
+    @item.save
   end
 
   private
