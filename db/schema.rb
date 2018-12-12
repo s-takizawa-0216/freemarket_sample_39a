1af3a24c80b88e0883b97ddb5d9bb9591f4fb6cf
yuuki <devel.yuuki@gmail.com>
2018-12-12 21:45:00 +0900

    Remove migratefile

--- db/schema.rb
+++ db/schema.rb
@@ -150,9 +150,6 @@ ActiveRecord::Schema.define(version: 2018_12_11_143732) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "provider"
-    t.string "uid"
-    t.string "token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
