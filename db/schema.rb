2c581babb848da3e4ba1df76815713ed2a28d4c9
yuuki <devel.yuuki@gmail.com>
2018-12-12 21:12:02 +0900

    Edit schema.rb
    Usersテーブルでproviderカラムがすでに存在しているためrake db:migrateできなかったためreset

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_12_11_143732) do
+
+  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "comment", null: false
@@ -97,8 +117,8 @@ ActiveRecord::Schema.define(version: 2018_12_11_143732) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "lcategory_id"
-    t.index ["lcategory_id"], name: "index_mcategories_on_lcategory_id"
+    t.bigint "lcategories_id"
+    t.index ["lcategories_id"], name: "index_mcategories_on_lcategories_id"
     t.index ["name"], name: "index_mcategories_on_name"
   end
 
@@ -106,29 +126,18 @@ ActiveRecord::Schema.define(version: 2018_12_11_143732) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "mcategory_id"
-    t.index ["mcategory_id"], name: "index_scategories_on_mcategory_id"
     t.index ["name"], name: "index_scategories_on_name"
   end
 
-  create_table "shippings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "shipping_methods", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "burden_fee", null: false
+    t.string "shipping_methods", null: false
     t.string "days_to_arrival", null: false
     t.string "prefecuture", null: false
     t.bigint "item_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["item_id"], name: "index_shippings_on_item_id"
-  end
-
-  create_table "ships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "burden_fee", null: false
-    t.string "days_to_arrival", null: false
-    t.string "prefecuture", null: false
-    t.bigint "item_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["item_id"], name: "index_ships_on_item_id"
+    t.index ["item_id"], name: "index_shipping_methods_on_item_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -169,7 +178,6 @@ ActiveRecord::Schema.define(version: 2018_12_11_143732) do
   add_foreign_key "comments", "items"
   add_foreign_key "comments", "users"
   add_foreign_key "credit_cards", "users"
-  add_foreign_key "shippings", "items"
-  add_foreign_key "ships", "items"
+  add_foreign_key "shipping_methods", "items"
   add_foreign_key "users_details", "users"
 end
