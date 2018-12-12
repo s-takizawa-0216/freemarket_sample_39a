c2ef6f582bbc50d2470b248bea41af6d877a1a09
h_yamano <josemarihiroki@icloud.com>
2018-12-12 12:47:18 +0900

    create_pickup-category&brand

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,14 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.order("created_at DESC")
+    @ladies = Item.where(lcategory_id: '1').limit(4)
+    @mens = Item.where(lcategory_id: '2').limit(4)
+    @baby = Item.where(lcategory_id: '3').limit(4)
+    @cosme = Item.where(lcategory_id: '6').limit(4)
+    @chanel = Item.where(brand: 'シャネル').limit(4)
+    @vuitton = Item.where(brand: 'ルイ ヴィトン').limit(4)
+    @supreme = Item.where(brand: 'シュプリーム').limit(4)
+    @nike = Item.where(brand: 'ナイキ').limit(4)
   end
 
   def show
