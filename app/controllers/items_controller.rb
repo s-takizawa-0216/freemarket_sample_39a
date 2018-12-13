c592ea9a533a3429edcccdc3890477ef45ad6697
shun <s.takizawa.0216@gmail.com>
2018-12-13 12:53:46 +0900

    edit items controller
    payjp用にコントローラーを編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,13 @@ class ItemsController < ApplicationController
   def show
   end
 
+  def payment
+    Payjp.api_key = 'sk_test_400205e7ce6cabc8a6ab9232'
+    charge = Payjp::Charge.create(
+    :amount => 3500,
+    :card => params['payjp-token'],
+    :currency => 'jpy',
+    )
+  end
+
 end
