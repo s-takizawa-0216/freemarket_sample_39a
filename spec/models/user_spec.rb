7a6d177930b510234bc31b5067ca035aace2535e
yuuki <devel.yuuki@gmail.com>
2018-12-17 19:31:31 +0900

    Edit user_spec.rb
    テスト
    ・passwordが空では登録できないこと

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,12 @@ RSpec.describe User, type: :model do
         another_user.valid?
         expect(another_user.errors[:email]).to include("has already been taken")
       end
+      #passwordが空では登録できないこと
+      it "is invalid with a password" do
+        user = build(:user, password: "")
+        user.valid?
+        expect(user.errors[:password]).to include("can't be blank")
+      end
       #nameが空では登録できないこと
       it "is invalid without a name" do
         user = build(:user, name: "")
@@ -34,7 +40,7 @@ end
 
 #テストの項目一覧
 # ・name,email,password,password_confirmationが存在すれば確認できること ok
-# ・emailが空では登録できないこと
+# ・emailが空では登録できないこと ok
 # ・emailが重複している場合登録できないこと ok
 # ・passwordが空では登録できないこと
 # ・passwordが6文字以上なら登録できること
