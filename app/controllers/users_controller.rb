43b10494ae1057035a4f0e8b4913e1efa5b85188
yuuki <devel.yuuki@gmail.com>
2018-12-17 15:15:35 +0900

    Edit users_controller.rb
    ログインページ用のアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,21 @@ class UsersController < ApplicationController
   def new
   end
 
+  def new1
+  end
+
+  def new2
+  end
+
+  def new3
+  end
+
+  def new4
+  end
+
+  def create
+  end
+
   def show
     @name = current_user.name
     @user = User.where(user_id: current_user.id)
